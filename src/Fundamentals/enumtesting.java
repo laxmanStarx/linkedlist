07a6506e34a6a2c02500c46408619c96d9489356
vaibhavlaxman <rabbitspend2103@gmail.com>
2026-05-11 19:24:51 +0530

    first commita

---
+++ src/Fundamentals/enumtesting.java
@@ -0,0 +1,15 @@
+package Fundamentals;
+
+enum Status{
+    Running, Pending, Stopped
+}
+
+public class enumtesting {
+    public static void main(String args[]) {
+        Status run = Status.Running;
+
+        System.out.println(run);
+
+    }
+
+}
